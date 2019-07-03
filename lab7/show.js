19938a11444be66b6499acbaed6812cd8c1fb7e5
whwayne <whwayne@gmail.com>
2019-07-03 14:39:35 -0500

    lab7 first commit

---
+++ lab7/show.js
@@ -0,0 +1,33 @@
+var accountInfoList =[];
+
+function createClick() {
+    var name = document.getElementById("account");
+    var deposit = document.getElementById("deposit");
+    const txt =document.getElementById("textarea");
+    const  obj = new Account(name.value, deposit.value);
+
+   // var accountInfoList =[];
+    accountInfoList.push(obj);
+    var str = "";
+    accountInfoList.forEach(myFunction);
+    function myFunction(obj) {
+        str = str +"Account name: " + obj.name + " Balance: " + obj.deposit + "\n"; 
+    }
+    txt.value = str;
+    name.value = "";
+    deposit.value = "";
+
+
+
+    function Account(name, deposit) {
+        this.name = name;
+        this.deposit = deposit;
+    }
+}
+
+function attachHandler(){
+    var createBtn = document.getElementById("create");
+    createBtn.onclick = createClick;
+  
+}
+window.onload = attachHandler;
