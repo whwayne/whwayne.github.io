f9c556768d58be0b6b69b47d03b5ea19d3ce1210
whwayne <whwayne@gmail.com>
2019-07-05 00:42:07 -0500

    evening commit

--- lab-7/maze_files/maze.js
+++ lab-7/maze_files/maze.js
@@ -1,7 +1,34 @@
 $(function(){
-    $("#boundary1").mouseover = (outBoundary1);
+    $('[class=boundary]').mouseover(function(){
+        $(this).addClass("youlose");
+    });
+
+    $("#end").click(function(){
+        if(($("#boundary1").hasClass("youlose"))||($("#boundary2").hasClass("youlose"))||
+           ($("#boundary3").hasClass("youlose"))||($("#boundary4").hasClass("youlose"))||
+           ($("#boundary5").hasClass("youlose"))){
+           // alert("Sorry you lost. :[");
+           $("h2").text("Sorry you lost. :[");
+        } else {
+           // alert("You win! :]");
+           $("h2").text("You win! :]");
+        }
+  
+    });
+
+    $("#start").click(function(){
+        $('[class=boundary]').removeClass("youlose");
+
+       
+    });
+
+    $("#maze").mouseout(function(){
+        $('[class=boundary]').addClass("youlose");
+        $("h2").text("Sorry you lost. :[");
+    });
+    
+
+
+
 });
 
-function outBoundary1(evt) {
-    $("#boundary1").addClass("youlose");
-}
